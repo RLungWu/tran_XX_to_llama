a48ece5ff720743131affa8feaf46a9e47ff020f
RLungWu <larry14789@gmail.com>
2025-02-13 17:43:32 +0800

    add some code

--- tran_qwen_to_llama.py
+++ tran_qwen_to_llama.py
@@ -1,4 +1,21 @@
+from typing import Any, Dict
+from collections import OrderedDict
+import os
+
+import torch
 from transformers import LlamaForCausalLM, LlamaTokenizer
+from tqdm import tqdm
+from safetensors import safe_open
+
 
 def tran_qwen_to_llama(model : str, path : str, target: str) -> str:
-    pass
+    config = path + "config.json"
+    
+    qwen_state_dict: Dict[str, torch.Tensor] = OrderedDict()
+    for filepath in tqdm(os.listdir(path), desc="Load Weights"):
+        if os.path.isfile(os.path.join(path, filepath)) as filepath.endswith(".safetensors"):
+            with safe_open(os.path.join(path, filepath), framework = "pt", device = "cpu") as f:
+                for key in f.keys():
+                    qwen_state_dict[key] = f.get_tensor(key)
+    
+
