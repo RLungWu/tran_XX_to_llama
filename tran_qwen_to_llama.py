a5b836e97ba066f65e33a1c46ea27a76a63d76b4
RLungWu <larry14789@gmail.com>
2025-02-14 12:17:18 +0800

    add some code

--- tran_qwen_to_llama.py
+++ tran_qwen_to_llama.py
@@ -18,4 +18,23 @@ def tran_qwen_to_llama(model : str, path : str, target: str) -> str:
                 for key in f.keys():
                     qwen_state_dict[key] = f.get_tensor(key)
     
+    
+    llama_state_dict: Dict[str, torch.Tensor] = OrderedDict()
+    torch_dtype = None
+    for key, value in tqdm(qwen_state_dict.items(), desc = "Convert format"):
+        if torch_dtype is None:
+            torch_dtype = value.dtype
+        
+        if "wte" in key:
+            llama_state_dict["model.embed_tokens.weight"] = value
+        elif "ln_f" in key:
+            llama_state_dict["model.norm.weight"] = value
+        else:
+            key = key.replace("transformer.h", "model.layers")
+            
+            if "attn.c_attn" in key:
+                proj_size = value.size(0) // 3
+                llama_state_dict[key.replace("attn.c_attn", "self_attn.q_proj")] = value[:proj_size, ...]
+                llama_state_dict[key.replace("attn.c_attn", "self_attn.k_proj")] = value
+    
 
